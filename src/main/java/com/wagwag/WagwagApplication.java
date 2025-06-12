09efc71ed3a04d57d277643fa9384f54c5c76300
YunSang Lee <qlqhwb@gmail.com>
2025-06-11 22:22:05 -0700

    Initial commit

---
+++ src/main/java/com/wagwag/WagwagApplication.java
@@ -0,0 +1,13 @@
+package com.wagwag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagwagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WagwagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wagwag/WagwagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagwag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagwagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
